29cf15af109c29273245c930f8b303c12f87de0c
finlayclark <s1629668@ed.ac.uk>
2022-06-14 15:17:00 +0100

    Typo - lambda to Alchemical_value

--- corelib/src/libs/SireMove/openmmfrenergyst.cpp
+++ corelib/src/libs/SireMove/openmmfrenergyst.cpp
@@ -1214,7 +1214,7 @@ void OpenMMFrEnergyST::initialise()
     // If in turn on receptor-ligand restraints mode, default value of lamrest needs to be lambda, because
     // the default value is used for the first nrg_freq timesteps before being set by updateOpenMMContextLambda
     if (perturbed_energies[8])
-        custom_link_bond->addGlobalParameter("lamrest", lambda);
+        custom_link_bond->addGlobalParameter("lamrest", Alchemical_value);
     // We are not in turn on receptor-ligand restraints mode - set lamrest to 1
     else custom_link_bond->addGlobalParameter("lamrest", 1);
 
@@ -1234,7 +1234,7 @@ void OpenMMFrEnergyST::initialise()
     custom_boresch_dist_rest->addPerBondParameter("equil_val");
     custom_boresch_dist_rest->setUsesPeriodicBoundaryConditions(true);
     if (perturbed_energies[8])
-        custom_boresch_dist_rest->addGlobalParameter("lamrest", lambda);
+        custom_boresch_dist_rest->addGlobalParameter("lamrest", Alchemical_value);
     // We are not in turn on receptor-ligand restraints mode - set lamrest to 1
     else custom_boresch_dist_rest->addGlobalParameter("lamrest", 1);
 
@@ -1245,7 +1245,7 @@ void OpenMMFrEnergyST::initialise()
     custom_boresch_angle_rest->addPerAngleParameter("equil_val");
     custom_boresch_angle_rest->setUsesPeriodicBoundaryConditions(true);
     if (perturbed_energies[8])
-        custom_boresch_angle_rest->addGlobalParameter("lamrest", lambda);
+        custom_boresch_angle_rest->addGlobalParameter("lamrest", Alchemical_value);
     // We are not in turn on receptor-ligand restraints mode - set lamrest to 1
     else custom_boresch_angle_rest->addGlobalParameter("lamrest", 1);
 
@@ -1257,7 +1257,7 @@ void OpenMMFrEnergyST::initialise()
     custom_boresch_dihedral_rest->addPerTorsionParameter("equil_val");
     custom_boresch_dihedral_rest->setUsesPeriodicBoundaryConditions(true);
     if (perturbed_energies[8])
-        custom_boresch_dihedral_rest->addGlobalParameter("lamrest", lambda);
+        custom_boresch_dihedral_rest->addGlobalParameter("lamrest", Alchemical_value);
     // We are not in turn on receptor-ligand restraints mode - set lamrest to 1
     else custom_boresch_dihedral_rest->addGlobalParameter("lamrest", 1);
 

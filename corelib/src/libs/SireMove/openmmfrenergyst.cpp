cd696a8d9e8ef1c90970f152eaefca68b1ba7abf
finlayclark <s1629668@ed.ac.uk>
2022-06-16 12:31:23 +0100

    Change value of lamrest when creating instances of restraint forces

--- corelib/src/libs/SireMove/openmmfrenergyst.cpp
+++ corelib/src/libs/SireMove/openmmfrenergyst.cpp
@@ -1219,21 +1219,7 @@ void OpenMMFrEnergyST::initialise()
     custom_link_bond->addPerBondParameter("kl");
     custom_link_bond->addPerBondParameter("dl");
     custom_link_bond->setUsesPeriodicBoundaryConditions(true);
-    // If in turn on receptor-ligand restraints mode, default value of lamrest needs to be lambda, because
-    // the default value is used for the first nrg_freq timesteps before being set by updateOpenMMContextLambda
-    if (perturbed_energies[8])
-    {
-        custom_link_bond->addGlobalParameter("lamrest", Alchemical_value);
-        if (Debug)
-            qDebug() << "Default value of lamrest for custom_link_bond has been set to: " << Alchemical_value;
-    }
-    // We are not in turn on receptor-ligand restraints mode - set lamrest to 1
-    else
-    {
-        custom_link_bond->addGlobalParameter("lamrest", 1);
-        if (Debug)
-            qDebug() << "Default value of lamrest for custom_link_bond has been set to: 1";
-    }
+    custom_link_bond->addGlobalParameter("lamrest", 1);
 
     /****************************************PERMANENT BOND LINK POTENTIAL*****************************/
 
@@ -1250,38 +1236,14 @@ void OpenMMFrEnergyST::initialise()
     custom_boresch_dist_rest->addPerBondParameter("force_const");
     custom_boresch_dist_rest->addPerBondParameter("equil_val");
     custom_boresch_dist_rest->setUsesPeriodicBoundaryConditions(true);
-    if (perturbed_energies[8])
-    {
-        custom_boresch_dist_rest->addGlobalParameter("lamrest", Alchemical_value);
-        if (Debug)
-            qDebug() << "Default value of lamrest for custom_boresch_dist_rest has been set to: " << Alchemical_value;
-    }
-    // We are not in turn on receptor-ligand restraints mode - set lamrest to 1
-    else
-    {
-        custom_boresch_dist_rest->addGlobalParameter("lamrest", 1);
-        if (Debug)
-            qDebug() << "Default value of lamrest for custom_boresch_dist_rest has been set to: 1";
-    }
+    custom_boresch_dist_rest->addGlobalParameter("lamrest", 1);
     /****************************************BORESCH ANGLE POTENTIAL*****************************/
 
     OpenMM::CustomAngleForce * custom_boresch_angle_rest = new OpenMM::CustomAngleForce("(lamrest^5)*0.5*force_const*(theta-equil_val)^2");
     custom_boresch_angle_rest->addPerAngleParameter("force_const");
     custom_boresch_angle_rest->addPerAngleParameter("equil_val");
     custom_boresch_angle_rest->setUsesPeriodicBoundaryConditions(true);
-    if (perturbed_energies[8])
-    {
-        custom_boresch_angle_rest->addGlobalParameter("lamrest", Alchemical_value);
-        if (Debug)
-            qDebug() << "Default value of lamrest for custom_boresch_angle_rest has been set to: " << Alchemical_value;
-    }
-    // We are not in turn on receptor-ligand restraints mode - set lamrest to 1
-    else 
-    {
-        custom_boresch_angle_rest->addGlobalParameter("lamrest", 1);
-        if (Debug)
-            qDebug() << "Default value of lamrest for custom_boresch_angle_rest has been set to: 1";
-    }
+    custom_boresch_angle_rest->addGlobalParameter("lamrest", 1);
 
     /****************************************BORESCH DIHEDRAL POTENTIAL*****************************/
 
@@ -1290,19 +1252,7 @@ void OpenMMFrEnergyST::initialise()
     custom_boresch_dihedral_rest->addPerTorsionParameter("force_const");
     custom_boresch_dihedral_rest->addPerTorsionParameter("equil_val");
     custom_boresch_dihedral_rest->setUsesPeriodicBoundaryConditions(true);
-    if (perturbed_energies[8])
-    {
-        custom_boresch_dihedral_rest->addGlobalParameter("lamrest", Alchemical_value);
-        if (Debug)
-            qDebug() << "Default value of lamrest for custom_boresch_dihedral_rest has been set to: " << Alchemical_value;
-    }
-    // We are not in turn on receptor-ligand restraints mode - set lamrest to 1
-    else
-    { 
-        custom_boresch_dihedral_rest->addGlobalParameter("lamrest", 1);
-        if (Debug)
-            qDebug() << "Default value of lamrest for custom_boresch_dihedral_rest has been set to: 1";
-    }
+    custom_boresch_dihedral_rest->addGlobalParameter("lamrest", 1);
 
     /****************************************PERMANENT BORESCH DISTANCE POTENTIAL*****************************/
 
@@ -3020,6 +2970,16 @@ void OpenMMFrEnergyST::initialise()
 
             if (molecule.hasProperty("permanent_linkbonds"))
             {
+                // If in turn on receptor-ligand restraints mode, must ensure lambda is set to Alchemical_value
+                // rather than the default of 1
+                if (molecule.hasProperty("turn_on_restraints_mode"))
+                {
+                    openmm_context->setParameter("lamrest", Alchemical_value); //Receptor-ligand restraints
+                    if (Debug)
+                    {
+                        qDebug() << "In turn-on receptor-ligand restraint mode: lamrest set to: "<< Alchemical_value;
+                    }
+                }
                 std::vector<double> custom_bond_link_par(3);
 
                 const auto linkprop = molecule.property("permanent_linkbonds").asA<Properties>();
@@ -3089,6 +3049,17 @@ void OpenMMFrEnergyST::initialise()
             {
                 found_solute = true; // We have found the solute, but before breaking we must also check
                                     // if there are Boresch angle and torsion restraints.
+
+                // If in turn on receptor-ligand restraints mode, must ensure lambda is set to Alchemical_value
+                // rather than the default of 1
+                if (molecule.hasProperty("turn_on_restraints_mode"))
+                {
+                    openmm_context->setParameter("lamrest", Alchemical_value); //Receptor-ligand restraints
+                    if (Debug)
+                    {
+                        qDebug() << "In turn-on receptor-ligand restraint mode: lamrest set to: "<< Alchemical_value;
+                    }
+                }
 
                 std::vector<double> custom_boresch_dist_par(2);
 
